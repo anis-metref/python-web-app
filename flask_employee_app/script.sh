31230c0701335947b2f7802e745ce749bb7676ab
anis-metref <122380920+anis-metref@users.noreply.github.com>
2024-06-09 22:50:11 +0200

    Update script.sh

--- flask_employee_app/script.sh
+++ flask_employee_app/script.sh
@@ -1,7 +1,1 @@
-# Initialiser la base de données
-sudo flask db init || true
-sudo flask db migrate -m "Migration initiale"
-sudo flask db upgrade
 
-# Lancer l'application Flask
-#python3 manage.py
